27b8df709486d5f76052404bf1ed1921871fabf1
Nando Bravo CH <nando.bravo@changoconsultora.com>
2025-05-20 23:46:06 -0300

    fix(main): arregle unos errores que evitaban la compilacion

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
 "use client";
 
 import HeroSidebar from "@/components/hero-sidebar";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useRef, useState } from "react";
-import { MdArrowOutward } from "react-icons/md";
 
-export default function Home() {
-  const [hovered, setHovered] = useState(false);
-  
-  const leaveTimeout = useRef<NodeJS.Timeout | null>(null); 
-  
-  const handleMouseLeave = () => {
-    leaveTimeout.current = setTimeout(() => {
-      setHovered(false); 
-    }, 1000);
-  };
-  const handleMouseEnter = () => {
-    if (leaveTimeout.current) {
-      clearTimeout(leaveTimeout.current); // Cancelar el timeout si vuelve a entrar antes del segundo
-    }
-    setHovered(true);
-  };
+export default function Home() {  
   return (
       <section className="section-container pt-4">
         <div className="hero-layout">
@@ -42,33 +24,11 @@ export default function Home() {
                 <div className="text-lg md:text-2xl h-full text-[hsl(var(--primary))] flex justify-center items-center  px-4">
                   <div className="relative">
                       <h2 className="inline rowdies-300 "><h2 className="inline afacad">Un  </h2>Diseño <h2 className="inline afacad"> que no solo se mira, sino que se siente con la mirada.</h2></h2> 
-                      {/* <Button
-                      onMouseEnter={handleMouseEnter}
-                      onMouseLeave={handleMouseLeave}
-                      className={`absolute left-0 bottom-0 z-100 border-white btn-shop-now-animation 
-                        flex items-center justify-start transition-all duration-1000 ease-in-out overflow-hidden 
-                        bg-[hsl(var(--primary))] text-white rounded-full left-[330px] gap-0
-                        ${hovered ? 'w-48 px-4' : 'w-12 pl-3'}
-                      `}
-                    >
-                      <MdArrowOutward className="w-5 h-5 min-w-[20px]" />
-                      <span className={`ml-2 whitespace-nowrap transition-opacity duration-300 ${hovered ? 'opacity-100' : 'opacity-0'}`}>
-                        COMPRAR AHORA
-                      </span>
-                    </Button> */}
-                   
                   </div>
                   
                 </div>
                 <div className="hero-curve hero-curve_three z-[-1]"></div>
                 <div className="hero-curve hero-curve_four"></div>
-
-
-                {/* <div className={`absolute bottom-0 bg-white right-[0px] z-[-1] w-[90px] h-[67px]  z-10 rounded-se-3xl ${hovered ? 'animate-slideRight' : 'animate-slideLeft'}`}>
-                  <div className="relative  ">
-                    <div className="hero-curve hero-curve_five"></div>
-                  </div>
-                </div> */}
               </div>
           
             </div>
