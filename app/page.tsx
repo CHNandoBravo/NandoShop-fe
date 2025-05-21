1cfbfd1b72f3c8a89471ab8a036945d285978016
Nando Bravo CH <nando.bravo@changoconsultora.com>
2025-05-20 23:39:36 -0300

    style(user-account-hero): :lipstick: Agregue responsive al hero

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,10 @@ export default function Home() {
   return (
       <section className="section-container pt-4">
         <div className="hero-layout">
-          <div className="hero-main  relative">
+          <div className="hero-main relative">
             <div className="absolute top-0 left-0 w-[170px] h-[50px] bg-white z-10 rounded-ee-3xl">
               <div className="playwrite h-full flex justify-center items-center"><i className="text-xl">NandoShop</i></div>
-              <div className="hero-curve hero-curve_one"></div>
+              <div className="hero-curve hero-curve_one max-md:shadow-none"></div>
               <div className="hero-curve hero-curve_two"></div>
             </div>
             <div className="h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] relative overflow-hidden rounded-3xl " >
@@ -38,11 +38,11 @@ export default function Home() {
                 alt="Picture of the author"        
                 className="object-cover"
               />
-              <div className="absolute bottom-0 left-0 w-[470px] h-[100px] bg-white z-10 rounded-se-3xl">
-                <div className="text-3xl h-full text-[hsl(var(--primary))] flex justify-center items-center  px-4">
+              <div className="absolute bottom-0 left-0 w-full md:w-[470px] h-[65px] md:h-[100px] bg-white z-10 max-sm:rounded-none md:rounded-se-3xl">
+                <div className="text-lg md:text-2xl h-full text-[hsl(var(--primary))] flex justify-center items-center  px-4">
                   <div className="relative">
-                      <h2 className="inline rowdies-300"><h2 className="inline afacad">Un  </h2>Diseño <h2 className="inline afacad"> que no solo se mira, sino que se siente con la mirada.</h2></h2> 
-                      <Button
+                      <h2 className="inline rowdies-300 "><h2 className="inline afacad">Un  </h2>Diseño <h2 className="inline afacad"> que no solo se mira, sino que se siente con la mirada.</h2></h2> 
+                      {/* <Button
                       onMouseEnter={handleMouseEnter}
                       onMouseLeave={handleMouseLeave}
                       className={`absolute left-0 bottom-0 z-100 border-white btn-shop-now-animation 
@@ -55,7 +55,7 @@ export default function Home() {
                       <span className={`ml-2 whitespace-nowrap transition-opacity duration-300 ${hovered ? 'opacity-100' : 'opacity-0'}`}>
                         COMPRAR AHORA
                       </span>
-                    </Button>
+                    </Button> */}
                    
                   </div>
                   
@@ -64,11 +64,11 @@ export default function Home() {
                 <div className="hero-curve hero-curve_four"></div>
 
 
-                <div className={`absolute bottom-0 bg-white right-[0px] z-[-1] w-[90px] h-[67px]  z-10 rounded-se-3xl ${hovered ? 'animate-slideRight' : 'animate-slideLeft'}`}>
+                {/* <div className={`absolute bottom-0 bg-white right-[0px] z-[-1] w-[90px] h-[67px]  z-10 rounded-se-3xl ${hovered ? 'animate-slideRight' : 'animate-slideLeft'}`}>
                   <div className="relative  ">
                     <div className="hero-curve hero-curve_five"></div>
                   </div>
-                </div>
+                </div> */}
               </div>
           
             </div>
